af9df9a8c15439b6ec4af8d20803aaac2592ac23
TravisScott584 <jackfuller1@yahoo.com>
2025-10-27 19:31:24 -0400

    Update salary_app2.py

--- salary_app2.py
+++ salary_app2.py
@@ -7,14 +7,14 @@ import numpy as np
 import matplotlib.ticker as mtick
 import os
 # Load model + encoders
-import gzip
-import pickle
+import os, gzip, pickle
 
 BASE_DIR = os.path.dirname(__file__)
 model_path = os.path.join(BASE_DIR, "salary_model_r_match.pkl.gz")
 
 with gzip.open(model_path, "rb") as f:
     model = pickle.load(f)
+
 
 
 saved = joblib.load(model_path)
